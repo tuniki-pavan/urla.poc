fa5f9bce4ee074cdbe13304fcc37246cf565564c
Pavan Tuniki <pavant@tscti.com>
2022-11-14 11:19:12 -0500

    fsdfdsf

--- src/test/java/usda/urla/poc/SearchInGoogleTest.java
+++ src/test/java/usda/urla/poc/SearchInGoogleTest.java
@@ -15,18 +15,15 @@ public class SearchInGoogleTest {
 	  // System Property for Chrome Driver   
       //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");  
       // Instantiate a ChromeDriver class.     
-      WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),DesiredCapabilities.chrome());  
-        
-         // Launch Website  
-      driver.navigate().to("http://www.google.com/");  
-      try{
-    	  Thread.sleep(5000);
-    	  }
-    	  catch(InterruptedException ie){
-    	  }
-       //Maximize the browser  
-        driver.manage().window().maximize();  
-        
-        driver.quit();
+		/*
+		 * WebDriver driver=new RemoteWebDriver(new
+		 * URL("http://localhost:4444/wd/hub"),DesiredCapabilities.chrome());
+		 * 
+		 * // Launch Website driver.navigate().to("http://www.google.com/"); try{
+		 * Thread.sleep(5000); } catch(InterruptedException ie){ } //Maximize the
+		 * browser driver.manage().window().maximize();
+		 * 
+		 * driver.quit();
+		 */
   }
 }
