c61dd9ac480160c77dfc2f49a5a6f344fe0f49fc
Pavan Tuniki <pavant@tscti.com>
2022-11-14 11:09:24 -0500

    fsdf

--- src/test/java/usda/urla/poc/SearchInJPTest.java
+++ src/test/java/usda/urla/poc/SearchInJPTest.java
@@ -19,22 +19,22 @@ public class SearchInJPTest {
       //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");  
       // Instantiate a ChromeDriver class.     
       //WebDriver driver=new ChromeDriver();  
-      WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),DesiredCapabilities.chrome());
-        
-         // Launch Website  
-      driver.navigate().to("http://www.javatpoint.com/");  
-        
-       //Maximize the browser  
-        driver.manage().window().maximize();  
-        
-        //Scroll down the webpage by 5000 pixels  
-      JavascriptExecutor js = (JavascriptExecutor)driver;  
-      js.executeScript("scrollBy(0, 5000)");   
-        
-       // Click on the Search button  
-      driver.findElement(By.linkText("Core Java")).click();  
-      
-      driver.quit();
+		/*
+		 * WebDriver driver=new RemoteWebDriver(new
+		 * URL("http://localhost:4444/wd/hub"),DesiredCapabilities.chrome());
+		 * 
+		 * // Launch Website driver.navigate().to("http://www.javatpoint.com/");
+		 * 
+		 * //Maximize the browser driver.manage().window().maximize();
+		 * 
+		 * //Scroll down the webpage by 5000 pixels JavascriptExecutor js =
+		 * (JavascriptExecutor)driver; js.executeScript("scrollBy(0, 5000)");
+		 * 
+		 * // Click on the Search button
+		 * driver.findElement(By.linkText("Core Java")).click();
+		 * 
+		 * driver.quit();
+		 */
 
   }
 }
